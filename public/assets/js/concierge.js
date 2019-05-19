b962d69ef71446625499a5d651c14d221c82e0c1
Manato1fg <manato1fg@gmail.com>
2019-05-19 11:56:33 +0900

    D

--- public/assets/js/concierge.js
+++ public/assets/js/concierge.js
@@ -211,12 +211,8 @@ function appendConciergeCallback(func) {
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Show the prompt
-    deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice
         .then((choiceResult) => {
