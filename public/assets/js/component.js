cdfefc26f15145adf49eca1cab8788f8d51efa88
Manato1fg <manato1fg@gmail.com>
2019-05-29 17:56:48 +0900

    うわん

--- public/assets/js/component.js
+++ public/assets/js/component.js
@@ -1,5 +1,12 @@
+A();
 function A() {
+    if(!getUserID()){
+        console.log("you have already logged in");
+        return;
+    }
     getBrowserFingerPrint(function (fingerprint) {
+        console.log(fingerprint);
+        
         window.localStorage.setItem("userid", fingerprint);
         postData(
             "https://suishosai-server-php.herokuapp.com/redirect4.php",
