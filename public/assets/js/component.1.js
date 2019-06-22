245be2cdabff93a91654a9f9a83e431f8d144d01
Manato1fg <manato1fg@gmail.com>
2019-06-23 08:13:16 +0900

    プライベートブラウザ

--- public/assets/js/component.1.js
+++ public/assets/js/component.1.js
@@ -12,16 +12,45 @@ function A(callback) {
                 var readyState = e.target.readyState;
                 var response = e.target.responseText;
                 if (status === 200 && readyState === 4) {
-                    window.localStorage.setItem("userid3", fingerprint);
-                    console.log(response);
-                    
-                    callback();
+                    // 利用例
+                    if (!storageAvailable('localStorage')) {
+                        
+                        alert("プライベートブラウザの使用はできません。あるいはCookieが有効になっていません");
+                        location.href = "https://suishosai.netlify.com";
+                        
+                    } else {
+                        window.localStorage.setItem("userid3", fingerprint);
+
+                        callback();
+                    }
                 }
             }
         )
     });
 }
 
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+    }
+}
 function postData(url, data, callback) {
 
     var xhr = new XMLHttpRequest();
